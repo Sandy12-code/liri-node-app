a363d457f0e6a2d02b7276a3f4aba647a450750e
Nala <sandy.enow1@gmail.com>
2019-10-17 21:37:03 -0400

    worked on both spotify and movie functions

--- liri.js
+++ liri.js
@@ -28,18 +28,6 @@ let bandsintown = (keys.bandsintown);
 var userInput = process.argv[2];
 var userQuery = process.argv.slice(3).join(" ");
 
-// console.log(keys);
-// create a new INSTANCE of the Spotify Library for use to use as a variable
-
-
-// var nodeArg = process.argv;
-// console.log(nodeArg);
-
-
-// console.log(process.argv[2]);
-
-// console.log(command);
-
 // APP LOGIC
 function userCommand(userInput, userQuery) {
     //make a decision based on the command
@@ -64,31 +52,34 @@ function userCommand(userInput, userQuery) {
 userCommand(userInput, userQuery);
 
 function concertThis() {
-    console.log(`\n-------\n\nsearching for...${userQuery}'s next show...`);
+    console.log(`\n - - - - -\n\n searching for...${userQuery}'s next show...`);
 
     //USE REQUEST AS OUR QUERY URL USING OUR USER QUERY VARIABLE AS THE PARAMETERS OF OUR SEARCH
-    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp" + bandsintown, function (response, body) {
-        if (!error && response.statuscode === 200) {
-            //Capture data and use JSON to format
-            let userBand = JSON.parse(body);
-            //Parse data and use for loop to access paths to data
-            if (userBand.lenght > 0) {
-                for (i = 0; i < 1; i++) {
-                    console.log(`\nHurray! That's for you...\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name} \nVenue Location: ${userBand[i].venue.latitude}.${userBand[i].venue.longitude} \nVenue City: ${userBand[i].venue.city}.${userBand[i].venue.country}`)
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp");
 
-                    // moment.js to format the date to MM/DD/YYYY
-                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
-                    console.log(`Date and Time: ${concertDate}\n\n---`);
-                };
 
-            }
-            else {
-                console.log('Band or concert not found!');
+    if (!error && response.statuscode === 200) {
+        //Capture data and use JSON to format
+        let userBand = JSON.parse(body);
+        //Parse data and use for loop to access paths to data
+        if (userBand.lenght > 0) {
+            for (i = 0; i < 1; i++) {
+                console.log(`\nHurray! That's for you...\n\n Artist: ${userBand[1].lineup[0]} \nVenue: ${userBand[1].venue.name} \nVenue Location: ${userBand[1].venue.latitude}.${userBand[1].venue.longitude} \nVenue City: ${userBand[1].venue.city}.${userBand[1].venue.country}`)
+
+                // moment.js to format the date to MM/DD/YYYY
+                let concertDate = moment(userBand[1].datetime).format("MM/DD/YYYY hh:00 A");
+                console.log(`Date and Time: ${concertDate}\n\n - - -`);
             };
-        };
 
-    });
-};
+        }
+        else {
+            console.log('Band or concert not found!');
+        };
+    };
+
+
+
+}
 
 
 function spotifyThisSong() {
@@ -110,26 +101,6 @@ function spotifyThisSong() {
         };
     });
 }
-// spotify.search({ type: 'track', query: command, limit: 5 })
-//     .then(function (response) {
-//         console.log(response);
-//         console.log(response.tracks);
-//         var song_name = response.tracks.items[0];
-//         console.log(song_name);
-//         var item_1_track_name = response.tracks.items[0].name;
-//         console.log(item_1_track_name);
-//         var artist = response.tracks.items[0].artists[0].name;
-//         console.log(artist);
-//         var prev_link = response.tracks.items[0].preview_url;
-//         console.log(prev_link);
-//         var album = response.album.external_urls[0].name;
-//         console.log(album);
-//     })
-//     .catch(function (err) {
-//         console.log(err);
-//     });
-
-// }
 
 
 function movieThis() {
@@ -137,35 +108,33 @@ function movieThis() {
     if (!userQuery) { userQuery = "mr nobody"; };
 
     //REQUEST WITH AXIOS TO THE OMDB API
-    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&apikey=trilogy").then(
-    function (error, response ) {
-        let ratingsArr = userMovie.Ratings;
-        if (ratingsArr + length > 2) {
-        }
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&apikey=trilogy")
+        .then(function (response) {
+            let data = response.data;
 
-        if (!error && response.statusCode === 200) {
-            console.log(`\nHurray! That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[i].Value}\nCountry:
-            ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
-        }
-        else {
-            return console.log("Movie not found. Error" + error)
-        };
-    });
+            // console.log('data =>', data);
+
+            let ratingsArr = data.Ratings;
+            console.log(ratingsArr);
+            // if (ratingsArr + length > 2) {
+            // }
+
+
+            console.log(`\n Hurray! That's for you...\n\n Title: ${data.Title}\n Cast: ${data.Actors}\n Released: ${data.Year}\nIMDb Rating: ${data.imdbRating}\nRotten Tomatoes Rating: ${data.Ratings[1].Value}\nCountry:
+            ${data.Country}\n Language: ${data.Language}\n Plot: ${data.Plot} \n\n- - - - -`)
+
+        })
+        .catch(function (error) {
+            if (error) {
+                console.log('error occured', error);
+            }
+        });
 };
-
-
-// axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-//     function (response) {
-//         console.log("The movie's rating is: " + response.data.imdbRating);
-//     })
-//     .catch(function (error) {
-//         if (error.response) {
-//         }
 
 
 function doThis() {
     fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {return console.log(erro); }
+        if (error) { return console.log(erro); }
         //Catch data and use the .SPLIT method to seperate objects within our new array
         let dataArr = data.split(",");
 
